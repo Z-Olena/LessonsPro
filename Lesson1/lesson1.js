b97ef1ffc95a9d4993323636e0a047fa6102909b
Z-Olena <galaktik2014@gmail.com>
2022-06-12 00:39:15 +0200

    initial commit

---
+++ Lesson1/lesson1.js
@@ -0,0 +1,8 @@
+alert ('Click"OK"')
+
+let name = 123456
+let name2 = 'IT School "Hillel"'
+let name3 = false
+console.log(name + ' ' + typeof name)
+console.log(name2 + ' ' + typeof name2)
+console.log(name3 + ' ' + typeof name3)
