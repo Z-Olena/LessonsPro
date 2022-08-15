9cd4d2fcdd9043aee5a563a48e9c8b573e798f08
Z-Olena <galaktik2014@gmail.com>
2022-08-15 17:19:49 +0200

    Added Lesson15

---
+++ Lesson15/lesson15.js
@@ -0,0 +1,75 @@
+async function createCard() {
+  let card = await fetch('https://rickandmortyapi.com/api/character/0,1,2,3,4,5,6,7,8,9,10')
+  let content = await card.json()
+  let location = await fetch('https://rickandmortyapi.com/api/location/1,2,3,4,5,6,7,8,9,10')
+  let content2 = await location.json()
+
+  let container = document.querySelector('.container')
+  
+  let key;
+  for (key in content) {
+    let livestatus = content[key].status
+    if (livestatus=='Dead'){
+      container.innerHTML += `
+    <div class="card">
+    <div class="card-info">
+      <div class="title">
+        <h1>${content[key].name}</h1>
+        <div class="status">
+        <div class="live-status">
+        <div class="live-status dead"></div>
+        </div>
+        <p>${content[key].species} - ${content[key].status}</p>
+      </div>
+      </div>
+      <div class="content">
+           <p>Last known location: 
+           ${content2[key].name}</p>
+      </div>
+    </div>
+    <div class="card-image">
+      <img src="${content[key].image}" alt="Img">
+    </div>
+  </div>
+    `
+     }
+     else{
+      container.innerHTML += `
+    <div class="card">
+    <div class="card-info">
+      <div class="title">
+        <h1>${content[key].name}</h1>
+        <div class="status">
+        
+          <div class="live-status"></div>
+          <p>${content[key].species} - ${content[key].status}</p>
+        </div>
+      </div>
+      <div class="content">
+           <p>Last known location: 
+           ${content2[key].name}</p>
+      </div>
+    </div>
+    <div class="card-image">
+      <img src="${content[key].image}" alt="Img">
+    </div>
+  </div>
+    `
+     }
+   }
+}
+createCard();
+
+
+
+
+
+
+// fetch('https://api.github.com/users/Z-Olena')
+// .then(data => data.json())
+// .then(data => {
+//   console.log(data)
+// })
+// .catch(error => {
+//   console.log(error);
+// })
